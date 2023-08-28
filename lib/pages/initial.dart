863009bdc00350414c063d1154c16697c3d7d80f
SolarFlaries <jlan@buffalo.edu>
2023-08-28 18:27:19 -0400

    Initalize startup screen

---
+++ lib/pages/initial.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class Welcome extends StatelessWidget {
+  const Welcome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+        child: Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height / 1.6,
+            child: Stack(
+              children: [
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.height / 1.6,
+                  decoration: BoxDecoration(color: Colors.white),
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.height / 1.6,
+                  decoration: BoxDecoration(
+                    color: Color(0xFF674AEF),
+                    borderRadius:
+                        BorderRadius.only(bottomRight: Radius.circular(70)),
+                  ),
+                  child: Center(child: Image.asset('1.png', scale: 0.8)),
+                )
+              ],
+            )));
+  }
+}
